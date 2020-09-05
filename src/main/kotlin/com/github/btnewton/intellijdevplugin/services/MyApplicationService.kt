3ae712601aba752c963da1d38dcb03bee8b5fcc9
GitHub Action <action@github.com>
2020-09-05 00:27:34 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/btnewton/intellijdevplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.btnewton.intellijdevplugin.services
+
+import com.github.btnewton.intellijdevplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/btnewton/intellijdevplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/btnewton/intellijdevplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.btnewton.intellijdevplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.btnewton.intellijdevplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
