50829ed67bdcc7ffa405c5c55da6699ba0d601c9
Brandt Newton <btnewton@uvm.edu>
2020-09-07 09:42:11 -0400

    removed events

--- src/main/kotlin/com/github/btnewton/breakreminder/services/GenericIntelliJListener.kt
+++ src/main/kotlin/com/github/btnewton/breakreminder/services/GenericIntelliJListener.kt
@@ -1,10 +1,7 @@
 package com.github.btnewton.breakreminder.services
 
-import com.intellij.codeInsight.completion.CompletionPhaseListener
 import com.intellij.execution.RunManagerListener
 import com.intellij.execution.RunnerAndConfigurationSettings
-import com.intellij.openapi.command.CommandEvent
-import com.intellij.openapi.command.CommandListener
 import com.intellij.openapi.components.service
 import com.intellij.openapi.vcs.changes.committed.VcsConfigurationChangeListener
 import com.intellij.openapi.vcs.update.UpdatedFilesListener
@@ -16,61 +13,34 @@ import com.intellij.openapi.wm.ex.ToolWindowManagerListener
 import com.intellij.problems.ProblemListener
 import com.intellij.task.ProjectTaskListener
 import com.intellij.task.ProjectTaskManager
-import com.intellij.xdebugger.XDebugProcess
-import com.intellij.xdebugger.XDebugSession
-import com.intellij.xdebugger.XDebuggerManagerListener
 import org.jetbrains.annotations.NotNull
 
 
 // naive way of detecting any interactions with IntelliJ
-class GenericIntelliJListener : BulkFileListener, ToolWindowManagerListener,
-CommandListener,
+class GenericIntelliJListener : BulkFileListener,
         RunManagerListener,
-        CompletionPhaseListener,
-        UpdatedFilesListener,
         ProblemListener,
-        XDebuggerManagerListener,
         ProjectTaskListener,
         VcsConfigurationChangeListener
 
 {
     override fun after(@NotNull events: List<VFileEvent?>) = workDone("VFS Event")
 
-    override fun toolWindowRegistered(id: String) = workDone("Tool Window Event")
-
-    override fun toolWindowUnregistered(id: String, toolWindow: ToolWindow) = workDone("Tool Window Event")
-
-    override fun completionPhaseChanged(isCompletionRunning: Boolean) = workDone("Completion Event")
-
-    override fun commandStarted(event: CommandEvent) = workDone("Command Event")
-
     override fun runConfigurationChanged(settings: RunnerAndConfigurationSettings) = workDone("Run Config Event")
 
-    override fun consume(t: MutableSet<String>?) = workDone("Updated files")
-
     override fun problemsAppeared(file: VirtualFile) = workDone("Problem Appeared")
+
     override fun problemsChanged(file: VirtualFile) = workDone("Problem Changed")
+
     override fun problemsDisappeared(file: VirtualFile) = workDone("Problem Disappeared")
 
     override fun beforeRunTasksChanged() = workDone("beforeRunTasksChanged")
 
-    override fun processStarted(debugProcess: XDebugProcess) = workDone("processStarted")
-
     override fun runConfigurationChanged(settings: RunnerAndConfigurationSettings, existingId: String?) = workDone("runConfigurationChanged")
-
-    override fun runConfigurationRemoved(settings: RunnerAndConfigurationSettings) = workDone("runConfigurationRemoved")
 
     override fun beginUpdate() = workDone("beginUpdate")
 
     override fun runConfigurationSelected(settings: RunnerAndConfigurationSettings?) = workDone("runConfigurationSelected")
-
-    override fun runConfigurationSelected() = workDone("runConfigurationSelected")
-
-    override fun currentSessionChanged(previousSession: XDebugSession?, currentSession: XDebugSession?) = workDone("currentSessionChanged")
-
-    override fun processStopped(debugProcess: XDebugProcess) = workDone("processStopped")
-
-    override fun finished(result: ProjectTaskManager.Result) = workDone("ProjectTaskManagerfinished")
 
     private fun workDone(eventSourceName: String) {
         println("Work Done: $eventSourceName")
